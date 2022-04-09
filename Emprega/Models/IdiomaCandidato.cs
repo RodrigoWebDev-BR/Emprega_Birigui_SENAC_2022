e37d39dcdcb5f0aebdaa4499128451b7e3d01641
John-Wedge <joao.lcjunior@outlook.com>
2022-04-08 21:46:48 -0300

    Criação Idioma Candidato

--- Emprega/Models/IdiomaCandidato.cs
+++ Emprega/Models/IdiomaCandidato.cs
@@ -1,14 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace Emprega.Models
 {
     public partial class IdiomaCandidato
     {
         public int Id { get; set; }
+        [Display(Name = "Idioma")]
         public int IdIdioma { get; set; }
         public string Nivel { get; set; } = null!;
-
+        [Display(Name = "Idioma Candidato")]
         public virtual Idioma IdIdiomaNavigation { get; set; } = null!;
     }
 }
---
+++ Emprega/Pages/Idioma_candidato/Edit.cshtml.cs
@@ -0,0 +1,79 @@
+﻿#nullable disable
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Emprega.Models;
+
+namespace Emprega.Pages.Idioma_candidato
+{
+    public class EditModel : PageModel
+    {
+        private readonly Emprega.Models.ApplicationDbContext _context;
+
+        public EditModel(Emprega.Models.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public IdiomaCandidato IdiomaCandidato { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            IdiomaCandidato = await _context.IdiomaCandidato
+                .Include(i => i.IdIdiomaNavigation).FirstOrDefaultAsync(m => m.Id == id);
+
+            if (IdiomaCandidato == null)
+            {
+                return NotFound();
+            }
+           ViewData["IdIdioma"] = new SelectList(_context.Idioma, "Id", "Nome");
+            return Page();
+        }
+
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see https://aka.ms/RazorPagesCRUD.
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Attach(IdiomaCandidato).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!IdiomaCandidatoExists(IdiomaCandidato.Id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return RedirectToPage("./Index");
+        }
+
+        private bool IdiomaCandidatoExists(int id)
+        {
+            return _context.IdiomaCandidato.Any(e => e.Id == id);
+        }
+    }
+}
