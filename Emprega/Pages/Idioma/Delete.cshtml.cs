20af08f2ba69e62a1ff59b3bb9d14dd3d32ce9b1
SalesPatrick <salespatrick77@gmail.com>
2022-04-08 22:14:51 -0300

    pagina idioma

---
+++ Emprega/Pages/Idioma/Delete.cshtml.cs
@@ -0,0 +1,59 @@
+﻿#nullable disable
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Emprega.Models;
+
+namespace Emprega.Pages.Idioma
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly Emprega.Models.ApplicationDbContext _context;
+
+        public DeleteModel(Emprega.Models.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Models.Idioma Idioma { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Idioma = await _context.Idioma.FirstOrDefaultAsync(m => m.Id == id);
+
+            if (Idioma == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Idioma = await _context.Idioma.FindAsync(id);
+
+            if (Idioma != null)
+            {
+                _context.Idioma.Remove(Idioma);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
---
+++ Emprega/Pages/Idioma/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿#nullable disable
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Emprega.Models;
+
+namespace Emprega.Pages.Idioma
+{
+    public class IndexModel : PageModel
+    {
+        private readonly Emprega.Models.ApplicationDbContext _context;
+
+        public IndexModel(Emprega.Models.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Models.Idioma> Idioma { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Idioma = await _context.Idioma.ToListAsync();
+        }
+    }
+}
